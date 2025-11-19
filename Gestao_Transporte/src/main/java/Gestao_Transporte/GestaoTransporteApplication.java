f73987558024779afb39aa1a872da07b94528521
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-11-19 19:45:19 -0300

    Fazendo Upload do projeto

---
+++ Gestao_Transporte/src/main/java/Gestao_Transporte/GestaoTransporteApplication.java
@@ -0,0 +1,13 @@
+package Gestao_Transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTransporteApplication.class, args);
+	}
+
+}
---
+++ Gestao_Transporte/src/test/java/Gestao_Transporte/GestaoTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package Gestao_Transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
