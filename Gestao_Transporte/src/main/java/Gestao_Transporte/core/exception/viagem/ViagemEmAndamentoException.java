71c21085b525295a48ad93bc350ab6c0dc76f41b
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-01-20 20:18:19 -0300

    Criada exceção para evitar finalizar uma viagem com status diferente de EM_ANDAMENTO

---
+++ Gestao_Transporte/src/main/java/Gestao_Transporte/core/exception/viagem/ViagemEmAndamentoException.java
@@ -0,0 +1,7 @@
+package Gestao_Transporte.core.exception.viagem;
+
+public class ViagemEmAndamentoException extends RuntimeException {
+  public ViagemEmAndamentoException(String message) {
+    super(message);
+  }
+}
